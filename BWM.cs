18c19d6c34a3dbc76b482f81a885c5fac2dfc0dc
Cennet Tugba UZDILLI <uzdillicennettugba@gmail.com>
2025-03-04 21:04:56 +0300

    Add files via upload

---
+++ BWM.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ödevvv
+{
+       public class BWM : Arabacs
+    {
+        public BWM(): base("Mercedes")
+        {
+        }
+
+        public void vitesTuru()
+        {
+            Console.WriteLine(marka + " → Otomatik viteslidir.");
+        }
+    }
+    
+}
+
