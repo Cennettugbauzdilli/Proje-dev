9ed1301d238cfccbfdfbac58fd819c48c2636183
Cennet Tugba UZDILLI <uzdillicennettugba@gmail.com>
2025-03-04 21:06:39 +0300

    Add files via upload

---
+++ Mersedes.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ödevvv
+{
+    public class Mersedes : Arabacs
+    {
+        public Mersedes() : base("Mercedes")
+        {
+        }
+
+        public  void vitesTuru()
+        {
+            Console.WriteLine(marka + " → Otomatik viteslidir.");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Ödevvv;
+using static Ödevvv.Mersedes;
+
+Console.WriteLine("Hello, World!");
+Arabacs a = new Arabacs();
+a.marka = "BMW";
+a.model = "X5";
+a.kapiSayisi = 4;
+a.pencereSayisi = 4;
+a.git("Mercedes", "XL");
+a.git(a.marka, a.model);
+
+BWM bm = new BWM();
+bm.markaModelYaz("Bmw", "X5");
+
+Mersedes me = new Mersedes();
+me.markaModelYaz("Mercedes", "XL");
