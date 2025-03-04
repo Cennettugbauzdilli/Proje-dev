31c564df094832e64ce2114a1aa3e0c46a3de039
Cennet Tugba UZDILLI <uzdillicennettugba@gmail.com>
2025-03-04 21:04:43 +0300

    Add files via upload

---
+++ Arabacs.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ödevvv
+{
+    public class Arabacs
+    {
+        //properties değişkenler
+        public string marka;
+        public string model;
+        public byte kapiSayisi;
+        public int pencereSayisi;
+
+        //METODLAR 
+        public void git(string markasi, string modeli)
+        {
+            Console.WriteLine(markasi + " " + modeli + " gidiyor...");
+        }
+
+
+        public void markaModelYaz(string markasi, string modeli)
+        {
+            Console.WriteLine("arabanın markası : " + markasi + " modeli : " + modeli);
+        }
+    }
+}
